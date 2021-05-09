138d91ed1168e6630bf9715f63bbc4b38bd9dd2a
denizutku <beydogandeniz@gmail.com>
2021-05-09 19:15:27 +0300

    default profile photo

--- db/migrate/20210430133619_add_image_url_to_users.rb
+++ db/migrate/20210430133619_add_image_url_to_users.rb
@@ -1,5 +1,5 @@
 class AddImageUrlToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :image_url, :string
+    add_column :users, :image_url, :string, :default => "https://lh3.googleusercontent.com/proxy/tPFNKX0WGx7epYdLRqThj1hiwajzhcygr5DgYoMw9MVhv-q1v8fP98bO-kEV2qZzRmlhYEb3wuWEu5xuPXCl8kAPpgID-p31bfb-VjkRGqGIufdjB5slK0hcSzgaso6IEXGQFHOgLUbNRS2JAIB95FApUVRc3X284w"
   end
 end
